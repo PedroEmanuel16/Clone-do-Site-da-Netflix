d5c492bcf977867fe503614627c3993a14e30281
PedroEmanuel16 <pedroemanuel070315@gmail.com>
2025-03-17 16:14:42 -0300

    innitial commits

---
+++ app/auth/page.tsx
@@ -0,0 +1,17 @@
+const AuthPage = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
+      <div className="h-full w-full bg-black lg:bg-black/50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="Logo" className="h-12" />
+        </nav>
+        
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
